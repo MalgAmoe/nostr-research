37cf2ee1e79212cbf58cf1f772e11d02ab68eeb3
MalgAmoe <malgamoe@protonmail.com>
2026-07-25 17:26:47 +0200

    Remove UI workflow tasks

--- workflow/tasks/014-playground-field-trial.validate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-test -s workflow/artifacts/first-playground-field-trial.md
-
-grep -q 'relay' workflow/artifacts/first-playground-field-trial.md
-grep -q 'checkpoint' workflow/artifacts/first-playground-field-trial.md
-
